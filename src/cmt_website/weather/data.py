c22ce123fddd4be86877d88283a5a991ca94308e
Harlan Shaw <thatdamnpillow@gmail.com>
2021-12-23 18:59:23 -0700

    WeatherFeatures written, added some beginning things to weather data

--- src/cmt_website/weather/data.py
+++ src/cmt_website/weather/data.py
@@ -1,42 +1,14 @@
-from typing import Iterable, Literal, Tuple, Union
-
 import pandas as pd
 from attr import define, field
+from cmt_website.weather.feature import WeatherFeature, WindDirection
 from cmt_website.weather.reader_interface import WeatherReaderInterface
 
 
 @define
 class WeatherData:
     reader: WeatherReaderInterface = field()
-    wind_direction: str = field(init=False, default="0")
-
-    def get_temperature(
-        self,
-        last: float = 0,
-        interval: Literal["month", "day", "hour", "minute", None] = None,
-    ) -> Iterable[float]:
-        pass
-
-    def get_wind_speed(
-        self,
-        last: float = 0,
-        interval: Literal["month", "day", "hour", "minute", None] = None,
-    ) -> Iterable[float]:
-        pass
-
-    def get_humidity(
-        self,
-        last: float = 0,
-        interval: Literal["month", "day", "hour", "minute", None] = None,
-    ) -> Iterable[float]:
-        pass
-
-    def get_pressure(
-        self,
-        last: float = 0,
-        interval: Literal["month", "day", "hour", "minute", None] = None,
-    ) -> Iterable[float]:
-        pass
-
-    def get_current_temperature(self) -> Tuple[float, float, float]:
-        pass
+    temperature: WeatherFeature = field(init=False)
+    wind_speed: WeatherFeature = field(init=False)
+    wind_direction: WindDirection = field(init=False)
+    humidity: WeatherFeature = field(init=False)
+    pressure: WeatherFeature = field(init=False)
--- src/cmt_website/weather/feature.py
+++ src/cmt_website/weather/feature.py
@@ -1,19 +1,96 @@
+from typing import Literal
+
 import pandas as pd
 from attr import define, field
 
 
 @define
 class WeatherFeature:
+    """Container and accessor for the data of a specific weather feature, such as
+    temperature"""
 
-    name: str = field()
-    data: pd.Series = field()
+    _data: pd.Series = field()
     current: float = field(init=False)
     minimum: float = field(init=False)
     maximum: float = field(init=False)
 
+    def update(self, new_data: pd.Series):
+        """Takes in new data to update the current WeatherFeature
+
+        Parameters
+        ----------
+        new_data : pd.Series
+            New data
+
+        """
+        self.current = new_data.tail(1).item()
+        self._data = new_data
+        last_day = self.last(amount=1, interval="day")
+        self.minimum = last_day.min()
+        self.maximum = last_day.max()
+
+    def __attrs_post_init__(self):
+        self.update(self._data)
+
+    def last(
+        self, amount: float = 1, interval: Literal["minute", "hour", "day"] = "day"
+    ) -> pd.Series:
+        """Retrieves all weather features from the specified interval of time
+
+        Parameters
+        ----------
+        amount : float
+            The number of intervals to retrieve from the weather feature
+        interval : Literal["minute", "hour", "day"]
+            The interval of time to look back, limited to minute hour or day.
+
+        Returns
+        -------
+        pd.Series
+            Timeseries of the weather feature from specified point in history
+            onward
+
+        Raises
+        ------
+        ValueError
+            Thrown if interval parameter given anything other than minute, hour
+            or day
+
+        """
+        time_intervals_allowed = ["minute", "hour", "day"]
+        if interval not in time_intervals_allowed:
+            raise ValueError(
+                f"Not an understandable time interval, expected one of {', '.join(time_intervals_allowed)}, received {interval}"
+            )
+        current = self._data.tail(1).index
+        offset = pd.Timedelta(value=amount, unit=interval)
+        delta_time = current - offset
+        return self._data[delta_time:]
+
 
 @define
-class Wind(WeatherFeature):
-    name: str = field(init=False, default="Wind")
-    direction: str = field(init=False)
-    direction_numeric: float = field(init=False)
+class WindDirection(WeatherFeature):
+    """Container and accessor for the data of wind direction, with distinct
+    accessor for the cardinal direction of the wind direction"""
+
+    @property
+    def cardinal(self):
+        """Matches the direction East from North to the cardinal directions"""
+        current = self.current
+        # I don't really like doing this, but cannot think of a better way yet
+        if current >= 337.5 or current < 22.5:
+            return "N"
+        if current >= 22.5 and current < 67.5:
+            return "NE"
+        if current >= 67.5 and current < 112.5:
+            return "E"
+        if current >= 112.5 and current < 157.5:
+            return "SE"
+        if current >= 157.5 and current < 202.5:
+            return "S"
+        if current >= 202.5 and current < 247.5:
+            return "SW"
+        if current >= 247.5 and current < 292.5:
+            return "W"
+        if current >= 292.5 and current < 337.5:
+            return "NW"
