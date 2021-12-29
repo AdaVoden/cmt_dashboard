dd70e7758e76d2298777a7ad5eb75e2b619d7b67
Harlan Shaw <thatdamnpillow@gmail.com>
2021-12-28 19:54:11 -0700

    Removed garbage file and converted shm.py to use classes

--- src/cmt_website/status/talon/shm.py
+++ src/cmt_website/status/talon/shm.py
@@ -1,14 +1,9 @@
-from collections import namedtuple
 from struct import Struct
-from typing import NamedTuple
 
 import sysv_ipc as sysv
 from attr import define, field
 from cmt_website.status.dome import Dome, DomeState, ShutterState
 from cmt_website.status.telescope import Telescope, TelescopeState
-
-# from cmt_website.status.dome import Dome, DomeState, ShutterState
-# from cmt_website.status.telescope import Telescope, TelescopeState
 
 # Formats of structs defined for Talon shared memory
 # Used to read from shared memory buffer
@@ -17,46 +12,42 @@ STATUS_FORMAT = "5ici3?2d2i"
 
 # Named tuple definitions for above formats to assist with readability
 # EOD means Epoch of Date
-Telescope_Position = namedtuple(
-    "Telescope_Positions",
-    [
-        "J2000_ra",
-        "J2000_dec",
-        "EOD_ra",
-        "EOD_ha",
-        "EOD_dec",
-        "altitude",
-        "azimuth",
-        "parallactic_angle",
-        "target_J2000_ra",
-        "target_J2000_dec",
-        "target_EOD_ra",
-        "target_EOD_ha",
-        "target_EOD_dec",
-        "target_altitude",
-        "target_azimuth",
-        "target_parallactic_angle",
-    ],
-)
-Status = namedtuple(
-    "Observatory_Status",
-    [
-        "telescope_state",
-        "ccd_temp_status",
-        "cam_state",
-        "cam_temp",
-        "target_cam_temp",
-        "filter",
-        "lights",
-        "autofocus",
-        "jogging",
-        "autodome",
-        "dome_azimuth",
-        "target_dome_azimuth",
-        "domestate",
-        "shutterstate",
-    ],
-)
+@define(slots=True)
+class TelescopePosition:
+    J2000_ra: float = field(converter=float)
+    J2000_dec: float = field(converter=float)
+    EOD_ra: float = field(converter=float)
+    EOD_ha: float = field(converter=float)
+    EOD_dec: float = field(converter=float)
+    altitude: float = field(converter=float)
+    azimuth: float = field(converter=float)
+    parallactic_angle: float = field(converter=float)
+    target_J2000_ra: float = field(converter=float)
+    target_J2000_dec: float = field(converter=float)
+    target_EOD_ra: float = field(converter=float)
+    target_EOD_ha: float = field(converter=float)
+    target_EOD_dec: float = field(converter=float)
+    target_altitude: float = field(converter=float)
+    target_azimuth: float = field(converter=float)
+    target_parallactic_angle: float = field(converter=float)
+
+
+@define(slots=True)
+class Status:
+    telescope_state: TelescopeState = field(converter=TelescopeState)
+    ccd_temp_status: int = field(converter=int)
+    cam_state: int = field(converter=int)
+    cam_temp: float = field(converter=float)
+    target_camp_temp: float = field(converter=float)
+    filter: str = field(converter=str)
+    lights: int = field(converter=int)
+    autofocus: bool = field(converter=bool)
+    jogging: bool = field(converter=bool)
+    autodome: bool = field(converter=bool)
+    dome_azimuth: float = field(converter=float)
+    target_dome_azimuth: float = field(converter=float)
+    domestate: DomeState = field(converter=DomeState)
+    shutterstate: ShutterState = field(converter=ShutterState)
 
 
 @define
@@ -64,13 +55,14 @@ class SHMStatusReader:
     """Using a given hex key, initializes and mediates a connection to a Linux shared memory segment that's used by the Talon/OCAAS system to share information between different daemons"""
 
     # Buffer offsets from 0, used to read specific pieces of data from the shared memory
+    # Calculated from the sizes the non-changing partings of telstatshm.h in libmisc
     telescope_offset: int = field(default=88)  # bytes
     status_offset: int = field(default=920)  # bytes
     SHM_hex_key: str = field(default="0x4e56361a")
     SHM_dec_key: int = field(init=False)
     _shared_memory: sysv.SharedMemory = field(init=False)
     _status: Status = field(init=False)
-    _telescope_position: Telescope_Position = field(init=False)
+    _telescope_position: TelescopePosition = field(init=False)
     _status_struct: Struct = field(init=False)
     _telescope_struct: Struct = field(init=False)
 
@@ -92,16 +84,15 @@ class SHMStatusReader:
         s_offset = self.status_offset
         t_struct = self._telescope_struct
         s_struct = self._status_struct
-        self._telescope_position = Telescope_Position._make(
+        self._telescope_position = TelescopePosition(
             t_struct.unpack_from(shm_buffer, offset=t_offset)
         )
-        self._status = Status._make(s_struct.unpack_from(shm_buffer, offset=s_offset))
+        self._status = Status(s_struct.unpack_from(shm_buffer, offset=s_offset))
 
     def read_telescope(self):
         self._read_all()
         position = self._telescope_position
         status = self._status
-        print(position)
         telescope_state = TelescopeState(status.telescope_state)
         return Telescope(
             state=telescope_state,
@@ -114,12 +105,11 @@ class SHMStatusReader:
     def read_dome(self):
         self._read_all()
         status = self._status
-        print(status)
         domestate = DomeState(status.domestate)
         shutterstate = ShutterState(status.shutterstate)
         return Dome(
             state=domestate,
-            shutterstate=ShutterState,
+            shutterstate=shutterstate,
             tracking=status.autodome,
             azimuth=status.dome_azimuth,
             target_azimuth=status.target_dome_azimuth,
